187c831a96da58afc3b259ac44c8f1c85d54e2d2
timokelder <52200561+timokelder@users.noreply.github.com>
2020-01-30 13:37:05 +0000

    cln

--- Notebooks/source_rfcv.r
+++
@@ -1,7 +0,0 @@
-structure(list("reliability.plot", structure(list(function (x, 
-    ...) 
-UseMethod("reliability.plot"), function (x, ...) 
-UseMethod("reliability.plot")), .Names = c("package:verification", 
-"")), c("package:verification", "namespace:verification"), c(TRUE, 
-FALSE), c(FALSE, TRUE)), .Names = c("name", "objs", "where", 
-"visible", "dups"), class = "getAnywhere")
